a748e6b1e7a49603d539dfac7019918b5ac400a6
noe <noe.reyes92@yahoo.com>
2021-08-18 21:14:28 -0500

    dropped db and created it again with proper Player attributes

--- db/migrate/20210818012613_create_players.rb
+++ db/migrate/20210818012613_create_players.rb
@@ -1,7 +1,9 @@
 class CreatePlayers < ActiveRecord::Migration[6.1]
   def change
     create_table :players do |t|
-
+      t.string :email
+      t.string :password_digest
+      t.string :username
       t.timestamps
     end
   end
