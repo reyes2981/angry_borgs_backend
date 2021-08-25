fc7220e70317c975d179496deb80e0c82302a5f2
noe <noe.reyes92@yahoo.com>
2021-08-25 11:12:35 -0500

    migrated score

--- db/migrate/20210825155627_create_scores.rb
+++ db/migrate/20210825155627_create_scores.rb
@@ -1,6 +1,8 @@
 class CreateScores < ActiveRecord::Migration[6.1]
   def change
     create_table :scores do |t|
+      t.integer :score
+      t.integer :player_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_012613) do
+ActiveRecord::Schema.define(version: 2021_08_25_155627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2021_08_18_012613) do
     t.string "email"
     t.string "password_digest"
     t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer "score"
+    t.integer "player_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
