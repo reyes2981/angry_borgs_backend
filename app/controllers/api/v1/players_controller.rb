cd2b2a5ebd491f8160027ea94a57c52f2d941232
noe <noe.reyes92@yahoo.com>
2021-08-28 12:49:15 -0500

    added create method to players controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -6,20 +6,33 @@ class Api::V1::PlayersController < ApplicationController
     end
 
     def create
-        player = Player.find_or_create_by(:email => params[:email])
-            if user && user.authenticate(params[:password])
+        player = Player.find_or_create_by(email: params['player']['email'],
+        username: params['player']['username'],
+        password: params['password']['password'],
+      )
+            if player.save
                 session[:player_id] = player.id
-                render json: session
+                render json: { 
+                    status success: 'success',
+                    player: player
+                }
             else
+                render json: { status: 500, errors: player.errors }
         end
     end
 
-   
+    def show
+        if player
+            render json:PlayerSerializer.new(player)
+        else 
+            render json: { messages: "Player could not be found"}
+        end
+    end
 
     private
 
     def player_params
-        params.require(:player).permit(:username, :email)
+        params.require(:player).permit(:username, :email, :password)
     end
 
 end
