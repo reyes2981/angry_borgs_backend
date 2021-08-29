13c4263c906611f0dbeb39042bf5d3e649b2cc3b
noe <noe.reyes92@yahoo.com>
2021-08-29 09:41:54 -0500

    refactored create/show methods in player controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -5,31 +5,39 @@ class Api::V1::PlayersController < ApplicationController
         render json: PlayerSerializer.new(players)
     end
 
+    def show
+        player = Playerplayer.find(params[:id])
+            if player
+               render json: {
+            player: player
+            }
+            else
+               render json: {
+               status: 500,
+               errors: ['player not found']
+             }
+            end
+    end
+
+
     def create
-        player = Player.find_or_create_by(email: params['player']['email'],
-        username: params['player']['username'],
-        password: params['password']['password'],
-      )
+        player = Player.find_or_create_by(player_params)
             if player.save
-                session[:player_id] = player.id
-                render json: { 
-                    status success: 'success',
+                login!
+                render json: {
+                    status: :created,
                     player: player
                 }
             else
-                render json: { status: 500, errors: player.errors }
+                render json: {
+                     status: 500, 
+                     errors: player.errors }
         end
     end
 
-    def show
-        if player
-            render json:PlayerSerializer.new(player)
-        else 
-            render json: { messages: "Player could not be found"}
-        end
-    end
+   
 
-    private
+    private # review private methods - I think they are methods that only developer can access? 
 
     def player_params
         params.require(:player).permit(:username, :email, :password)
